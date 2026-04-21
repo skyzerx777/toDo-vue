38bf20a1268a12849340e1a812fc7188ab652dbb
Yevhenii Hrebenshchykov <yevheniihrebenshchykov@gmail.com>
2026-04-21 13:30:22 +0300

    Status and expiration filters

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,19 +1,47 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import TodoItem from './TodoItem.vue';
 import type { ITodo } from './types';
 
 const { todoArray } = defineProps<{ todoArray: ITodo[] }>();
+type StatusesType = 'All' | 'Completed' | 'Incompleted';
+const statuses: StatusesType[] = ['All', 'Completed', 'Incompleted'];
+const hideExpired = ref(false);
+const status = ref<StatusesType>('All');
 
 function removeTodo(index: number) {
 	todoArray.splice(index, 1);
 }
 
-const reversedTodoArray = computed<ITodo[]>(() => [...todoArray].reverse());
+const filteredTodoArray = computed(() => {
+	return filterTodoArray(todoArray);
+});
+
+function filterTodoArray(array: ITodo[]) {
+	let filteredArray = [...array];
+
+	if (hideExpired.value === true) {
+		filteredArray = filteredArray.filter(item => !item.overdue);
+	}
+	if (status.value === 'Completed') {
+		filteredArray = filteredArray.filter(item => item.completed);
+	}
+	if (status.value === 'Incompleted') {
+		filteredArray = filteredArray.filter(item => !item.completed);
+	}
+	return filteredArray;
+}
+const reversedTodoArray = computed<ITodo[]>(() =>
+	[...filteredTodoArray.value].reverse(),
+);
 </script>
 
 <template>
 	<div>
+		<label><input type="checkbox" v-model="hideExpired" />Hide expired</label>
+		<select v-model="status">
+			<option v-for="status in statuses">{{ status }}</option>
+		</select>
 		<TodoItem
 			v-for="(todo, index) in reversedTodoArray"
 			:key="todo.id"
